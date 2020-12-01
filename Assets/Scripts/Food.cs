b644f3bcf1aed7d95a3ab5b58a1bce682c8d0103
Kléber de Oliveira Andrade <pdjkleber@gmail.com>
2020-11-30 23:04:58 -0300

    Create Food.cs

---
+++ Assets/Scripts/Food.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Food : MonoBehaviour
+{
+    public delegate void OnFoodEatenHandler();
+    public static OnFoodEatenHandler OnFoodEaten;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Snake"))
+        {
+            if (OnFoodEaten != null)
+                OnFoodEaten();
+
+            Destroy(gameObject);
+        }
+    }
+}
+
+
